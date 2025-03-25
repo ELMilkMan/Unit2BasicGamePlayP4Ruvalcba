e9298770b6388c8e95623fc041182d1dc529c69c
ELMilkMan <730254@student.dusd.net>
2025-03-25 09:54:32 -0700

    made some more changes to my project
    fixed issue
    
    on Lesson 2.2 - Food Flight: part 2

--- Unit2BasicGamePlayRuvalcabaP4/Assets/Scripts/PlayerControler.cs
+++ Unit2BasicGamePlayRuvalcabaP4/Assets/Scripts/PlayerControler.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class PlayerControler : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 17.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +18,19 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
+        if(transform.position.x < -xRange)
         {
-            (transform.position = new Vector3(-10, transform.position.y, transform.position.z);
-        } 
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        
+     
     }
 }
